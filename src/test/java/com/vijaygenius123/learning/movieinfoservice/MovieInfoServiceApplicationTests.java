bccfa6896609d92b18acd9ab1ba94f82c35d8cb1
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2020-10-04 21:03:45 +0530

    Initial commit

---
+++ src/test/java/com/vijaygenius123/learning/movieinfoservice/MovieInfoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vijaygenius123.learning.movieinfoservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieInfoServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
